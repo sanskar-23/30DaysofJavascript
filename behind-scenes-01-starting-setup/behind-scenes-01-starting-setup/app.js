0793af26b9feac4495dafce0a6cf6c9a24f29138
Sanskar Kapoor <76424866+sanskar-23@users.noreply.github.com>
2021-09-06 21:15:23 +0530

    Add files via upload

---
+++ behind-scenes-01-starting-setup/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,16 @@
+var name = 'MAX';
+
+
+if(name == 'MAX') {
+  var hobbies = ['sports', 'cookies'];
+  console.log(hobbies);
+}
+
+function greet() {
+  var age = 30;
+  var name = "sanskar"
+  console.log(name, age, hobbies);
+}
+
+console.log(hobbies);
+greet();
