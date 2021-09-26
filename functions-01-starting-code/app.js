42a0ae697c807d7339e6fbe2bd7318ac3eb09025
Sanskar Kapoor <76424866+sanskar-23@users.noreply.github.com>
2021-09-27 01:19:42 +0530

    Add files via upload

---
+++ functions-01-starting-code/app.js
@@ -0,0 +1,15 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+
+const start = function() {
+  console.log('The game is loading..');
+};
+function startGame() {
+  console.log('The game is loading..');
+}
+
+
+
+
+console.dir(startGame);
+startGameBtn.addEventListener('click', start);
