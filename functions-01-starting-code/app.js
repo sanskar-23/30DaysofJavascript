c5c08b65ed9194ea34a841b28ed3c5fdfb858441
Sanskar Kapoor <76424866+sanskar-23@users.noreply.github.com>
2021-09-27 18:59:33 +0530

    Update app.js

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -1,15 +1,55 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = 'ROCK';
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
-const start = function() {
-  console.log('The game is loading..');
+let gameIsRunning = false;
+
+const getPlayerChoice = function() {
+  const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`).toUpperCase();
+  if(selection!==ROCK && selection!==PAPER && selection !==SCISSORS) {
+    alert(`Invalid Choice! We choose ${DEFAULT_USER_CHOICE} for you`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
 };
-function startGame() {
-  console.log('The game is loading..');
+
+
+const getComputerChoice = function() {
+  const randomValue = Math.random();
+  if(randomValue < 0.34) {
+    return ROCK;
+  } else if(randomValue < 0.67) {
+    return PAPER;
+    }
+    else {
+      return SCISSORS;
+    }
 }
 
+const getWinner = function(cChoice, pChoice) {
+  if(cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if(cChoice === ROCK && pChoice === PAPER || cChoice === PAPER && pChoice === SCISSORS || cChoice === SCISSORS && pChoice === ROCK) {
+    return RESULT_PLAYER_WINS
+  } else {
+    return RESULT_COMPUTER_WINS;
+  }
+}
 
-
-
-console.dir(startGame);
-startGameBtn.addEventListener('click', start);
+startGameBtn.addEventListener('click', function() {
+  if(gameIsRunning){
+    return;
+  }
+  gameIsRunning = true;
+  console.log('The game is loading..');
+  const playerSelection = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerSelection);
+  console.log(winner);
+});
