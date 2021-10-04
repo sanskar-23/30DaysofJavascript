0b4978a6e750cd8f7b6647271073a89c8c0b50cd
sanskar-23 <76424866+sanskar-23@users.noreply.github.com>
2021-10-04 10:25:36 +0530

    array destructing

--- arrays-01-starting-code/app.js
+++ arrays-01-starting-code/app.js
@@ -1,79 +1,160 @@
-// // // const numbers = [1,2,3];
-// // // console.log(numbers);
-// // //
-// // // // const moreNumbers = Array('Hi', 'Welcome');
-// // // // console.log(moreNumbers);
-// // // //
-// // // // const yetMoreNumbers = Array.of(1,2,3,4);
-// // // // console.log(yetMoreNumbers);
-// // //
-// // // const list = document.querySelectorAll('li');
-// // // console.log(list);
-// // //
-// // // const moreNumbers = Array.from(list);
-// // // console.log(moreNumbers);
-// // //
-// // // const hobbies = ['cooking', 'cricket', 'sex'];
-// // // const personalData = ['20', '30', '40'];
-// // //
-// // // const analytics = [[1,2,3], [4,5,6], [7,8,9]];
-// // //
-// // // for(const data of analytics) {
-// // //   console.log(data);
-// // // }
-// // //
-// // // console.log(hobbies[1]);
-// //
-// //
-// // const hobbies = ['sports', 'cooking', 'singing'];
-// // hobbies.push('reading');
-// // hobbies.unshift('coading');
-// // hobbies.pop();
-// // console.log(hobbies);
-// //
-// // hobbies[1] = 'sanskar';
-// // console.log(hobbies);
-// //
-// // hobbies.splice(1,0, 'Good food');
-// // console.log(hobbies);
-// //
-// // hobbies.splice(-1,1);
-// // console.log(hobbies);
-//
-//
-// const testResults = [1,5.3,1.5,10.99,-5,10];
+// const numbers = [1, 2, 3];
+// console.log(numbers);
+
+// // const moreNumbers = Array(5, 2);
+// // console.log(moreNumbers);
+
+// // const yetMoreNumbers = Array.of(1, 2);
+// // console.log(yetMoreNumbers);
+
+// const listItems = document.querySelectorAll('li');
+// console.log(listItems);
+
+// const arrayListItems = Array.from(listItems);
+// console.log(arrayListItems);
+
+// const hobbies = ['Cooking', 'Sports'];
+// const personalData = [30, 'Max', {moreDetail: []}];
+
+// const analyticsData = [[1, 1.6], [-5.4, 2.1]];
+
+// for (const data of analyticsData) {
+//   for (const dataPoint of data) {
+//     console.log(dataPoint);
+//   }
+// }
+
+// console.log(personalData[1]);
+
+// const hobbies = ['Sports', 'Cooking'];
+// hobbies.push('Reading');
+// hobbies.unshift('Coding');
+// const poppedValue = hobbies.pop();
+// hobbies.shift();
+// console.log(hobbies);
+
+// hobbies[1] = 'Coding';
+// // hobbies[5] = 'Reading'; // rarely used
+// console.log(hobbies, hobbies[4]);
+
+// hobbies.splice(1, 0, 'Good Food');
+// console.log(hobbies);
+
+// const removedElements = hobbies.splice(-2, 1);
+// console.log(hobbies);
+
+// const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
 // // const storedResults = testResults.slice(2);
-// const storedResults = testResults.concat([89,90,91]);
-// testResults.push(69);
-//
-// console.log(testResults, storedResults);
-// console.log(testResults.indexOf(-5));
-//
-// const personDetails = [{name: 'sanskar'}, {college: 'TIET'}];
-// console.log(personDetails.indexOf({name: 'sanskar'}));
-//
-// const manuel = personDetails.find( (person, idx, persons) => {
-//   return person.name === 'sanskar';
+// const storedResults = testResults.concat([3.99, 2]);
+
+// testResults.push(5.91);
+
+// console.log(storedResults, testResults);
+// console.log(testResults.indexOf(1.5));
+
+// console.log(testResults.includes(10.99));
+// console.log(testResults.indexOf(10.99) !== -1);
+
+// const personData = [{ name: 'Max' }, { name: 'Manuel' }];
+// console.log(personData.indexOf({ name: 'Manuel' }));
+
+// const manuel = personData.find((person, idx, persons) => {
+//   return person.name === 'Manuel';
 // });
-//
-// manuel.name = 'sam';
-//
-// console.log(manuel);
-//
-// const maxIndex = personDetails.findIndex((person, idx, persons) => {
-//   return person.name === 'sam';
+
+// manuel.name = 'Anna';
+
+// console.log(manuel, personData);
+
+// const maxIndex = personData.findIndex((person, idx, persons) => {
+//   return person.name === 'Max';
 // });
+
 // console.log(maxIndex);
-//
-// console.log(testResults.includes(1.5));
 
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
+// const taxAdjustedPrices = [];
 
-const prices = [10,20,30,40,50];
-const tax = 0.19;
+// // for (const price of prices) {
+// //   taxAdjustedPrices.push(price * (1 + tax));
+// // }
 
-  const taxAdjustedPrices = prices.map((price, idx, prices) => {
-  const priceobj = {index: idx, taxAdjustedPrices: price * (1 + tax)};
-  return priceobj;
-});
+// prices.forEach((price, idx, prices) => {
+//   const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
+//   taxAdjustedPrices.push(priceObj);
+// });
 
-console.log(prices, taxAdjustedPrices);
+// console.log(taxAdjustedPrices);
+
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
+
+// const taxAdjustedPrices = prices.map((price, idx, prices) => {
+//   const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
+//   return priceObj;
+// });
+
+// // console.log(prices, taxAdjustedPrices);
+
+// const sortedPrices = prices.sort((a, b) => {
+//   if (a > b) {
+//     return -1;
+//   } else if (a === b) {
+//     return 0;
+//   } else {
+//     return 1;
+//   }
+// });
+// // console.log(sortedPrices.reverse());
+// console.log(sortedPrices);
+
+// const filteredArray = prices.filter(p => p > 6);
+
+// console.log(filteredArray);
+
+// // let sum = 0;
+
+// // prices.forEach((price) => {
+// //   sum += price
+// // });
+
+// // console.log(sum);
+
+// const sum = prices.reduce((prevValue, curValue) => prevValue + curValue, 0);
+
+// console.log(sum);
+
+// const data = 'new york;10.99;2000';
+
+// const transformedData = data.split(';');
+// transformedData[1] = +transformedData[1];
+// console.log(transformedData);
+
+// const nameFragments = ['Max', 'Schwarz'];
+// const name = nameFragments.join(' ');
+// console.log(name);
+
+// const copiedNameFragments = [...nameFragments];
+// nameFragments.push('Mr');
+// console.log(nameFragments, copiedNameFragments);
+
+// console.log(Math.min(...prices));
+
+// const persons = [{ name: 'Max', age: 30 }, { name: 'Manuel', age: 31 }];
+// const copiedPersons = persons.map(person => ({
+//   name: person.name,
+//   age: person.age
+// }));
+
+// persons.push({ name: 'Anna', age: 29 });
+// persons[0].age = 31;
+
+// console.log(persons, copiedPersons);
+
+const nameData = ['Max', 'Schwarz', 'Mr', 30];
+// const firstName = nameData[0];
+// const lastName = nameData[1];
+
+const [ firstName, lastName, ...otherInformation ] = nameData;
+console.log(firstName, lastName, otherInformation);
